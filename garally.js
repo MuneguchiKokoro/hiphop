0a274c183c89caf074252e5f75a2ca3003708353
MuneguchiKokoro <47618233+MuneguchiKokoro@users.noreply.github.com>
2021-12-09 10:42:12 +0900

    Update garally.js

--- garally.js
+++ garally.js
@@ -12,4 +12,14 @@ $(function(){
         return false;
     });
 
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+
 });
