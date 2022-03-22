b94cad68a420d1f58f5ee97980f9bf24df1a3102
MuneguchiKokoro <47618233+MuneguchiKokoro@users.noreply.github.com>
2022-03-22 13:29:03 +0900

    Update garally.js

--- garally.js
+++ garally.js
@@ -1,4 +1,15 @@
-$(function(){
+$(function () {
+    
+    var h = $(window).height();
+    $('#wrap').css('display','none');
+    $('#loader-bg ,#loader').height(h).css('display','block');
+    });
+    
+    $(window).load(function () { //全ての読み込みが完了したら実行
+    $('#loader-bg').delay(900).fadeOut(800);
+    $('#loader').delay(600).fadeOut(300);
+    $('#wrap').css('display', 'block');
+
 
     // モーダルウィンドウ
     $("main").append("<div id='glay_layer'></div><div id='over_layer'></div>"); // main要素の最後に glay_layerとover_layerを配置
